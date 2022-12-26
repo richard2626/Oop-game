fc0b1c28cdf0c1ed4467367fd083da3f9383344d
richard2626 <li.chichih>
2022-12-26 23:10:53 +0800

    test7

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+// g++ -c main.cpp -I /opt/homebrew/Cellar/sfml/2.5.1_2/include
+// g++ main.o -o sfml-app -L /opt/homebrew/Cellar/sfml/2.5.1_2/lib -lsfml-graphics -lsfml-window -lsfml-system
+
+int main()
+{
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
